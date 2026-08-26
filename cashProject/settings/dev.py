d63e07db3cbb2b8417539288f8a5b2d3a580f557
nawabsakib5 <nawabsakib5@gmail.com>
2026-08-27 02:20:24 +0600

    Redsign Project

--- cashProject/settings/dev.py
+++ cashProject/settings/dev.py
@@ -1,4 +1,5 @@
 from .base import *
+from decouple import config
 
 DEBUG = True
 
@@ -6,7 +7,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
